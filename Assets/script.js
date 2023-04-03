6a70f0f5da046413589073daaaad576737228908
joeyrodo <joeyrodo@gmail.com>
2023-04-03 15:54:53 -0400

    Completed save and display function

--- Assets/script.js
+++ Assets/script.js
@@ -2,26 +2,45 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  for (i = 0; i < 9; i++) {
+    document.getElementsByClassName('time-block')[i].children[1].value = localStorage.getItem(i);
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $( ".saveBtn" ).on('click', function() {
+    console.log("button clicked");
+    var thisTextArea = this.parentElement.childNodes[3].value;
+    var storageId = this.parentElement.id;
+    if (thisTextArea) {
+      console.log(thisTextArea);
+      console.log(storageId);
+      localStorage.setItem(storageId, thisTextArea);
+    }
+    else {
+      console.log("Nothing here!");
+    }
+  });
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+
   // TODO: Add code to display the current date in the header of the page.
   const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   currentDayText = document.getElementById("currentDay");
   currentDayText.textContent = weekdays[dayjs().get('day')] + ", " + months[dayjs().get('month')] + " " + dayjs().get('date') + ", " + dayjs().get('year');
 });
+
